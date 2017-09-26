f24cac133bad84e1224ad0232e1c95998b476659
Scott Lindeneau <slindeneau@gmail.com>
2017-09-25 20:14:48 -0700

    Read in filedata

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import utils
-
+import sys
 
 def main():
+	attendFileName = sys.argv[1]
+	statsFileName = sys.argv[2]
+	attendData = utils.readCSV(attendFileName)
+	statsData = utils.readCSV(statsFileName)
+
+	for row in attendData:
+		print(row)
+	for row in statsData:
+		print(row)
+
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
 import csv
-
+import urllib.request
 def readCSV(filename):
 	rows = []
 	with open(filename) as csvfile:
-    csvreader = csv.reader(csvfile)
-    for row in csvreader:
-    	rows.append(row)
-  return rows
-  
+		csvreader = csv.reader(csvfile)
+		for row in csvreader:
+			rows.append(row)
+	return rows
+
+
+def getGitHubStats(username):
+	urllib.request.urlopen("http://example.com/foo/bar").read()
